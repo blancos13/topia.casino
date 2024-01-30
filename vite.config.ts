175f01ba0874a30fefc47621f314b74289a0b173
ZeroByte4701 <94198465+ZeroByte4701@users.noreply.github.com>
2024-01-31 01:42:09 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+const ENV_PREFIX = ['VITE_']
+
+export default defineConfig(() => ({
+  envPrefix: ENV_PREFIX,
+  server: { port: 4001, host: false },
+  assetsInclude: ["**/*.glb"],
+  define: { 'process.env.ANCHOR_BROWSER': true },
+  plugins: [
+    react({ jsxRuntime: 'classic' }),
+  ],
+}))
